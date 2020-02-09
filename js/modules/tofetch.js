036c88a7e7825a809c6f776049433dc39ef10bd3
Emerson Leite <emerson@unna.eng.br>
2020-02-08 21:11:47 -0300

    202002082110

--- js/modules/tofetch.js
+++ js/modules/tofetch.js
@@ -1,30 +1,54 @@
 export default class ToFetch {
   constructor(url) {
+    this.element = document.querySelector(".data");
+    this.dataHeader = document.querySelector(".header");
     this.url = url;
   }
 
   async anyFetch() {
-    this.response = await fetch(this.url);
-    this.data = await this.response.json();
-    //return this.data.BRL.sell;
-    return this.data;
+    this.resposta = await fetch(this.url);
+    // retornando a url
+    //console.log(this.resposta.url);
+    // interando sobre o headers da resposta
+    /*  for (let [key, value] of this.resposta.headers) {
+      console.log(`${key}: ${value}`);
+    } */
+    // Recuperando o tipo de dado da resposta
+    //console.log(this.resposta.headers.get("content-type"));
+    //console.log(this.resposta.headers);
+    this.dados = await this.resposta.json();
+    // mostrando dados...
+    console.log(this.dados);
+    //return this.dados;
+    this.renderData(this.dados);
+    this.renderHeader(this.resposta.headers);
   }
-  
-  get mostrarData() {
-    return this.valor = {
-      valor: this.anyFetch(),
+
+  // renderizando dados na tela - até segundo nível de objetos
+  renderData(data) {
+    for (let key in data) {
+      console.log(typeof data[key]);
+      if (typeof data[key] !== "object") {
+        this.element.innerHTML += `<p>${key} : ${data[key]}</p>`;
+      } else {
+        this.element.innerHTML += `<p>${key}</p>`;
+        for (let key2 in data[key]) {
+          this.element.innerHTML +=  `<p>${key2} - ${data[key][key2]}</p>`;
+        }
+        this.element.innerHTML += `<br>`;
+      }
     }
   }
 
-  renderHeaders(){
-    
+  renderHeader(header) {
+    for (let [key, value] of header) {
+      this.dataHeader.innerHTML += `<p>${key} : ${value}</p>`;
+    }
   }
 
+  // inicializando
   init() {
     this.anyFetch();
-    //this.mostrarData();
     return this;
   }
 }
-  
-
--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 import ToFetch from './modules/tofetch.js';
 
-const tofetch = new ToFetch('https://blockchain.info/ticker');
+/*  const tofetch = new ToFetch('https://blockchain.info/ticker');
+tofetch.init(); */ 
+ 
+const tofetch = new ToFetch('https://api.myjson.com/bins/xi3hi');
 tofetch.init();
 
 
-const tofetch02 = new ToFetch('https://api.myjson.com/bins/xi3hi');
-tofetch02.init();
+
+
