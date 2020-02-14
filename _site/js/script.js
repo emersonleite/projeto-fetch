253001366b8bba3d8c62de41a9e87212d618b0cc
Emerson Leite <emerson.bl@gmail.com>
2020-02-13 21:40:20 -0300

    202002132140

---
+++ _site/js/script.js
@@ -0,0 +1,17 @@
+import ToFetch from "./modules/tofetch.js";
+
+// inicializando
+const tofetch = new ToFetch();
+//tofetch.anyFetch("https://blockchain.info/ticker");
+tofetch.init();
+console.log(tofetch.init());
+
+
+/* https://blockchain.info/ticker */
+
+/* "https://api.myjson.com/bins/xi3hi" */
+
+/* https://reqres.in/api/users?page=2 */
+
+/* https://api.github.com/users/emersonleite */
+
